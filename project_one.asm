5e5b01983b150bea088f59b16426d51dadd447e4
Prabin Sapkota <professional_prabin@yahoo.com>
2018-10-14 17:28:22 -0400

    Added instructions to print first char of my name

--- project_one.asm
+++ project_one.asm
@@ -1,5 +1,9 @@
-# MIPS instructions to print my name "Prabin Sapkota"
-# one character at a time using only one register and
+# MIPS instructions to print my name Prabin Sapkota
+# one character at a time using only one register
 
 .text
 main:
+	#Print the first character of my name P
+	li	$a0, 'P' #P
+	li	$v0, 11
+	syscall
