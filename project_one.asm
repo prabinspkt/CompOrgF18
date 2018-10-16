dcdd46969cab78420162ed452d96a51c0eea06c1
Prabin Sapkota <professional_prabin@yahoo.com>
2018-10-15 23:33:24 -0400

    Added instructions to print r character

--- project_one.asm
+++ project_one.asm
@@ -3,7 +3,11 @@
 
 .text
 main:
-	#Print the first character of my name P
-	li	$a0, 'P' #P
+	#Print the character P
+	li	$a0, 'P'
+	li	$v0, 11
+	syscall
+	# Print the character r
+	addi	$a0, $a0, 34
 	li	$v0, 11
 	syscall
