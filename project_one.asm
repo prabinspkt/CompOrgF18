80e6aefbafd80e6914c10069c2f6c56f6aa751d0
Prabin Sapkota <professional_prabin@yahoo.com>
2018-10-15 23:36:27 -0400

    Added instructions to terminate the program

--- project_one.asm
+++ project_one.asm
@@ -7,7 +7,11 @@ main:
 	li	$a0, 'P'
 	li	$v0, 11
 	syscall
-	# Print the character r
+	#Print the character r
 	addi	$a0, $a0, 34
 	li	$v0, 11
 	syscall
+	#Terminate the program
+exit:	li	$v0, 10
+	syscall
+	
